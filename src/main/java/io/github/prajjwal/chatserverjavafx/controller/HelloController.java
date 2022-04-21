318eb6c70ba305e732e12f42bc609df38b829cec
Prajjwal <prajjwal.pachauri_cs19@gla.ac.in>
2022-04-21 19:23:02 +0530

    change names and ui

---
+++ src/main/java/io/github/prajjwal/chatserverjavafx/controller/HelloController.java
@@ -0,0 +1,23 @@
+package io.github.prajjwal.chatserverjavafx.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+    @FXML
+    public Label startServerText;
+
+    @FXML
+    public Label statusText;
+
+    @FXML
+    public Button severStartButton;
+
+    @FXML
+    public void onStartButtonClick() {
+
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module io.github.prajjwal.chatserverjavafx {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.slf4j;
 
     opens io.github.prajjwal.chatserverjavafx to javafx.fxml;
     exports io.github.prajjwal.chatserverjavafx;
+    exports io.github.prajjwal.chatserverjavafx.controller;
+    opens io.github.prajjwal.chatserverjavafx.controller to javafx.fxml;
 }
