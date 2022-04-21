f17a8d336233d5ef88182c8319fba2a28555b6e7
Prajjwal <prajjwal.pachauri_cs19@gla.ac.in>
2022-04-21 19:23:34 +0530

    create DuplicateUsernameException.java class

---
+++ src/main/java/io/github/prajjwal/chatserverjavafx/exception/DuplicateUsernameException.java
@@ -0,0 +1,9 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri(cypher)
+Date: 21-04-2022
+Time: 7:22 PM
+File: DuplicateUsernameException.java */
+package io.github.prajjwal.chatserverjavafx.exception;
+
+public class DuplicateUsernameException {}
