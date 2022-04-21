6c3a0b960ca91995441fe744148190923d9661f2
Prajjwal <prajjwal.pachauri_cs19@gla.ac.in>
2022-04-21 19:25:12 +0530

    create User.java class

---
+++ src/main/java/io/github/prajjwal/chatserverjavafx/message/User.java
@@ -0,0 +1,10 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri(cypher)
+Date: 21-04-2022
+Time: 7:24 PM
+File: User.java */
+package io.github.prajjwal.chatserverjavafx.message;
+
+public class User {
+}
